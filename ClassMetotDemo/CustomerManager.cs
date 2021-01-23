5cd232ff32a06c32d45cf01bfd217639c4f0aa03
Gülden Arslanoğlu <gulden.arslanoglu@boun.edu.tr>
2021-01-23 14:21:28 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " " + customer.Surname + " adlı müşteri eklendi."); 
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " " + customer.Surname + " adlı müşteri silindi.");
+        }
+        
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.Id = 1;
+            customer.Name = "Gülden";
+            customer.Surname = "Sönmez";
+            customer.Address = "Bahçelievler";
+
+            Customer customer1 = new Customer();
+            customer1.Id = 2;
+            customer1.Name = "İsmail";
+            customer1.Surname = "Sönmez";
+            customer1.Address = "Bahçelievler";
+
+            Customer customer2 = new Customer { Id = 3, Name = "Batuhan", Surname = "Sönmez", Address = "Bahçeliever" };
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer);
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+
+            Console.WriteLine("------------------------------------");
+
+            customerManager.Delete(customer);
+        }
+    }
+}
